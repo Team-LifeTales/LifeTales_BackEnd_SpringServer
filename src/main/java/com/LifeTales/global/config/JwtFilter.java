c5fd392e57687704819570cba98c9e6f63e8a8db
Sejin-999 <76008226+Sejin-999@users.noreply.github.com>
2023-08-26 20:04:41 +0900

    #6 jwt - accredited 설정완료..

--- src/main/java/com/LifeTales/global/config/JwtFilter.java
+++ src/main/java/com/LifeTales/global/config/JwtFilter.java
@@ -2,7 +2,10 @@ package com.LifeTales.global.config;
 
 import com.LifeTales.domain.user.domain.UserRole;
 import com.LifeTales.domain.user.service.UserService;
+import com.LifeTales.global.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,13 +19,30 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 @RequiredArgsConstructor
+@Slf4j
 public class JwtFilter extends OncePerRequestFilter {
 
     private final UserService userService;
     private final String secretKey;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String userName = ""; //공백에서 찾아서 넣어줌..
+        final String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
+        log.info("authorization : {}" ,authorization);
+        String userName = ""; //공백에서 찾아서 넣어줌.
+
+        if(authorization == null){
+            log.error("인증토큰 확인하세요");
+            filterChain.doFilter(request , response);
+            return;
+        }
+        //token
+        String token = authorization;
+
+        if(JwtUtil.isExpired(token,secretKey)){
+            log.info("Token 기간만료");
+            filterChain.doFilter(request , response);
+            return;
+        }
 
         UsernamePasswordAuthenticationToken authenticationToken =
                 new UsernamePasswordAuthenticationToken(userName , null , List.of(new SimpleGrantedAuthority("FAMILY_MEMBER")));
--- src/main/java/com/LifeTales/global/util/JwtUtil.java
+++ src/main/java/com/LifeTales/global/util/JwtUtil.java
@@ -8,6 +8,10 @@ import java.util.Date;
 
 public class JwtUtil {
 
+    public static boolean isExpired(String token , String secretKey){
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getExpiration().before(new Date());
+    }
+
     public static  String createJwtToken(String id , String secretKey , long expiredMs){
         Claims claims = Jwts.claims();
         claims.put("id" , id);
